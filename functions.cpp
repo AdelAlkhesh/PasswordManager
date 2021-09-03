38952a5cb79013e09109c231f012aa63d66946ca
Shen <splinteredsanity@outlook.com>
2021-09-03 10:10:10 +0200

    Added function to store generated pass

--- functions.cpp
+++ functions.cpp
@@ -5,8 +5,8 @@ void SaveToFile(string path, string email, string password, string accountName) 
   my_file.open(path, std::ios_base::app);
 
   if (my_file) {
-    my_file << setw(25) << std::left << accountName << setw(40) << std::left
-            << email << setw(15) << password;
+    my_file << setw(25) << std::left << accountName << setw(30) << std::left
+            << email << setw(25) << password;
     my_file << endl;
   }
   my_file.close();
@@ -44,8 +44,9 @@ void Welcome() {
     ofstream my_file("accounts.txt");
 
     if (my_file) {
-      my_file << setw(25) << std::left << "ACCOUNT" << setw(40) << std::left
-              << "EMAIL" << setw(15) << "PASSWORD";
+      my_file << setw(25) << std::left << "ACCOUNT" << setw(30) << std::left
+              << "EMAIL" << setw(25) << "PASSWORD";
+      my_file << endl;
       my_file << endl;
       signUp();
     }
@@ -53,18 +54,37 @@ void Welcome() {
   }
 }
 
-string PassGenerate(int length) {
+void storePassword(string password) {
+  string email, accountName;
+  cout << "Enter an email:" << endl;
+  cin >> email;
+  cout << "Enter an account name:" << endl;
+  cin >> accountName;
+  SaveToFile("accounts.txt", email, password, accountName);
+}
+
+void PassGenerate() {
   const char alphanum[] =
       "0123456789!@#$%^_-*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   int string_length = sizeof(alphanum) - 1;
   string password;
+  int length;
+  cout << "Enter the desired password length ";
+  cin >> length;
   srand(time(NULL));
   cout << "Generated Password:" << endl;
   for (int i = 0; i < length; i++) {
     password += alphanum[rand() % string_length];
   }
+  string answer;
   cout << password << endl;
-  return password;
+  cout << endl;
+  cout << "Would you like to store this password?(Y/N)" << endl;
+  cin >> answer;
+  if (answer == "Y")
+  {
+    storePassword(password);
+  }
+ 
 }
 
-void storePassword() {}
